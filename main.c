a9ed9eefeba578244767ea9be74de39bd3a98b3c
안준영 (JunYeong Ahn) <hewas321@gmail.com>
2020-09-22 04:12:30 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+	return 0;
+
+}
+
+int main(void) {
+	int num;
+
+	printf("정수:");
+	scanf("%d", &num);
+	printf("입력 값: %d\n", num);
+	return 0;
+}
